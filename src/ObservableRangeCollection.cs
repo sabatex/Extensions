4cb1588edb99926f819a6dd71db643c1108a075b
sabatex <serhiy.lakas@hotmail.com>
2022-01-26 09:57:10 +0200

    update to net6.0

--- src/ObservableRangeCollection.cs
+++ src/ObservableRangeCollection.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+// Copyrigth (c) Serhiy Lakas 2020
+
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
