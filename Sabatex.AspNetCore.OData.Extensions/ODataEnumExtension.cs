2c5330659c536e51a5f149651f49537e6a08f314
sabatex <serhiy.lakas@hotmail.com>
2023-09-29 14:32:10 +0300

    add Sabatex.AspNetCore.OData.Extensions

---
+++ Sabatex.AspNetCore.OData.Extensions/ODataEnumExtension.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.OData.ModelBuilder;
+
+namespace Sabatex.AspNetCore.OData.Extensions
+{
+    public static class ODataEnumExtension
+    {
+        public static void AddEnumTypeAsInt(this ODataConventionModelBuilder builder, Type enumType)
+        {
+            var e = builder.AddEnumType(enumType);
+            foreach (var item in Enum.GetValues(enumType))
+            {
+                var m = e.AddMember((Enum)item);
+                m.Name = ((int)item).ToString();
+            }
+        }
+    }
+}
---
+++ Sabatex.AspNetCore.OData.Extensions/README.md
@@ -0,0 +1,15 @@
+﻿# AspNetCore OData Extensions
+
+## change enum routing from string to int 
+
+---
+using Sabatex.AspNetCore.OData.Extensions
+
+IEdmModel GetEdmModel()
+{
+    var builder = new ODataConventionModelBuilder();
+    ...
+    builder.AddEnumTypeAsInt(typeof(Enum Type));
+    return builder.GetEdmModel();
+}
+---
