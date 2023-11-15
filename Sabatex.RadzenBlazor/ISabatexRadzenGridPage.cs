d5028f0d7224d304b97982771315ce043b7a8a07
sabatex <serhiy.lakas@hotmail.com>
2023-11-15 12:57:20 +0200

    remove Sabatex.RadzenBlazor  then moved to separate repository

--- Sabatex.RadzenBlazor/ISabatexRadzenGridPage.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Components.Web;
-
-namespace Sabatex.RadzenBlazor
-{
-    public interface ISabatexRadzenGridPage{
-        public Task AddButtonClick(MouseEventArgs args);
-        public bool IsGridRO{get;set;}
-    }    
-}
