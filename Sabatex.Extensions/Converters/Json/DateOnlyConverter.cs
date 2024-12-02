aa246e1c3c579152948c3c808cb99968ab2cac88
sabatex <serhiy.lakas@hotmail.com>
2024-12-02 17:09:52 +0200

    Add IEmailSender interface and Date converters

---
+++ Sabatex.Extensions/Converters/Json/DateOnlyConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Sabatex.Extensions.Converters.Json;
+
+public class DateOnlyConverter : JsonConverter<DateOnly>
+{
+    private const string DateFormat = "yyyy-MM-dd";
+
+    public override DateOnly Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        return DateOnly.ParseExact(reader.GetString() ?? String.Empty, DateFormat, CultureInfo.InvariantCulture);
+    }
+
+    public override void Write(Utf8JsonWriter writer, DateOnly value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.ToString(DateFormat, CultureInfo.InvariantCulture));
+    }
+}
---
+++ Sabatex.Extensions/Converters/Json/TimeOnlyConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Sabatex.Extensions.Converters.Json;
+
+public class TimeOnlyConverter : JsonConverter<TimeOnly>
+{
+    private const string DateTimeFormat = "0001-01-01THH:mm:ss";
+    private const string TimeFormat = "HH:mm:ss.FFFFFFF";
+    
+    public override TimeOnly Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        return TimeOnly.ParseExact(reader.GetString() ?? String.Empty, DateTimeFormat, CultureInfo.InvariantCulture);
+    }
+    
+    public override void Write(Utf8JsonWriter writer, TimeOnly value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.ToString(DateTimeFormat, CultureInfo.InvariantCulture));
+    }
+}
