7fa495af21d0e7dc734158ca0a77562d076732a5
sabatex <serhiy.lakas@hotmail.com>
2025-02-03 10:48:26 +0200

    add UserRoleAbstraction

---
+++ Sabatex.Core/UserRoleAbstraction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabatex.Core
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public abstract class UserRoleAbstraction
+    {
+        /// <summary>
+        /// Administrator role
+        /// </summary>
+        public const string Administrator = "Administrator";
+        /// <summary>
+        /// Get Avaliable roles
+        /// </summary>
+        /// <returns></returns>
+        public abstract IEnumerable<string> GetAvaliableRoles();
+    }
+}
