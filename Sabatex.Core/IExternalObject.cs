f1f3bb61b5a8b946d2a05c0699f0535f6af2a5a4
sabatex <serhiy.lakas@hotmail.com>
2025-02-24 09:26:35 +0200

    add IExternalObject

---
+++ Sabatex.Core/IExternalObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabatex.Core;
+
+public interface IExternalObject
+{
+    /// <summary>
+    /// mark object as external 
+    /// </summary>
+    public bool IsExternal { get; set; }
+    /// <summary>
+    /// External Id (number,string,uuid)
+    /// indexed for exchange
+    /// </summary>
+    public string ExternalId { get; set; }
+}
